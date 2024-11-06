36084242752e0f05e595cc1f4047216fc0703584
simonweigold <simonw750@gmail.com>
2024-11-06 17:14:10 +0100

    Configure variable import

---
+++ cold_case_analyzer/config.py
@@ -0,0 +1,9 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Config:
+    AIRTABLE_API_KEY = os.getenv('AIRTABLE_API_KEY')
+    AIRTABLE_BASE_ID = os.getenv('AIRTABLE_BASE_ID')
+    AIRTABLE_CD_TABLE = os.getenv('AIRTABLE_CD_TABLE')
