6c0053301fd92b6a1b2cabc0c408432fd74d61b5
simonweigold <simonw750@gmail.com>
2024-11-08 17:23:18 +0100

    Remove class from config

--- cold_case_analyzer/config.py
+++ cold_case_analyzer/config.py
@@ -3,7 +3,6 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-class Config:
-    AIRTABLE_API_KEY = os.getenv('AIRTABLE_API_KEY')
-    AIRTABLE_BASE_ID = os.getenv('AIRTABLE_BASE_ID')
-    AIRTABLE_CD_TABLE = os.getenv('AIRTABLE_CD_TABLE')
+AIRTABLE_API_KEY = os.getenv('AIRTABLE_API_KEY')
+AIRTABLE_BASE_ID = os.getenv('AIRTABLE_BASE_ID')
+AIRTABLE_CD_TABLE = os.getenv('AIRTABLE_CD_TABLE')
