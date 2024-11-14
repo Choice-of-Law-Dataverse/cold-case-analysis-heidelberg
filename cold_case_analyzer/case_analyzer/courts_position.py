db1e5af39c3e44a63e021cb71c5ca1b0c7e73008
simonweigold <simonw750@gmail.com>
2024-11-14 15:22:01 +0100

    Rm redundant empty line

--- cold_case_analyzer/case_analyzer/courts_position.py
+++ cold_case_analyzer/case_analyzer/courts_position.py
@@ -2,7 +2,6 @@ from llm_handler.model_access import prompt_model
 
 def extract_courts_position(text, prompt, issue, model):
     prompt_position = f"""{prompt}
-
                 {issue}
     
                 Here is the text of the Court Decision:
