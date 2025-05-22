72832e2e5cdf6045b0c6774ae7adfa4651123139
Simon Weigold <simonw750@gmail.com>
2025-05-22 13:39:12 +0200

    Only test the col extractor

--- cold_case_analyzer_agent/main.py
+++ cold_case_analyzer_agent/main.py
@@ -17,12 +17,12 @@ def run_cold_case_analysis(state: AppState):
     state_col_section = run_col_section_extraction(state)
     print("\n--- STATE AFTER COL SECTION EXTRACTION ---")
     print(state_col_section)
-    state_theme_classification = run_theme_classification(state_col_section)
-    print("\n--- STATE AFTER THEME CLASSIFICATION ---")
-    print(state_theme_classification)
-    state_final = run_analysis(state_theme_classification)
+    #state_theme_classification = run_theme_classification(state_col_section)
+    #print("\n--- STATE AFTER THEME CLASSIFICATION ---")
+    #print(state_theme_classification)
+    #state_final = run_analysis(state_theme_classification)
     # Return the final state
-    return state_final
+    #return state_final
 
 initial_state = {
     "full_text": SAMPLE_COURT_DECISION,
